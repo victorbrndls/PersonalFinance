efd7cbe1840787745557780a62f6f2e8f1b7068e
Victor B <haristolhogamer@gmail.com>
2022-09-26 21:16:38 -0300

    Add title to charts page

--- app/src/main/java/com/victorbrndls/pfs/ui/chart/ChartsRoute.kt
+++ app/src/main/java/com/victorbrndls/pfs/ui/chart/ChartsRoute.kt
@@ -1,10 +1,17 @@
 package com.victorbrndls.pfs.ui.chart
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.victorbrndls.pfs.R
 import com.victorbrndls.pfs.ui.chart.expense_category.ExpenseByCategoryChart
@@ -38,25 +45,45 @@ private fun ChartsScreen(
                 .padding(innerPadding)
                 .consumedWindowInsets(innerPadding)
         ) {
-            Column(modifier = modifier) {
+            Column(
+                modifier = modifier.verticalScroll(state = rememberScrollState())
+            ) {
+                ChartTitle(text = stringResource(id = R.string.chart_title_income_and_net_savings))
                 IncomeAndNetSavingsChart(
                     modifier = Modifier
                         .fillMaxWidth()
                         .landscapeRatio()
+                        .padding(bottom = 12.dp)
                 )
 
+                ChartTitle(text = stringResource(id = R.string.chart_title_savings_rate))
                 SavingsRateChart(
                     modifier = Modifier
                         .fillMaxWidth()
                         .landscapeRatio()
+                        .padding(bottom = 12.dp)
                 )
 
                 ExpenseByCategoryChart(
                     modifier = Modifier
                         .fillMaxWidth()
                         .landscapeRatio()
+                        .padding(bottom = 12.dp)
                 )
             }
         }
     }
+}
+
+@Composable
+private fun ChartTitle(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Medium,
+        modifier = modifier.padding(horizontal = 12.dp)
+    )
 }
--- app/src/main/java/com/victorbrndls/pfs/ui/home/HomeQuickMenu.kt
+++ app/src/main/java/com/victorbrndls/pfs/ui/home/HomeQuickMenu.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,7 +15,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.victorbrndls.pfs.R
-import com.victorbrndls.pfs.ui.designsystem.theme.Purple40
 import com.victorbrndls.pfs.ui.designsystem.theme.White
 
 @Composable
@@ -22,10 +23,6 @@ internal fun HomeQuickMenu(
     onNavigateToTransactions: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val buttonColors = ButtonDefaults.buttonColors(
-        containerColor = Purple40
-    )
-
     // TODO remove fling
     LazyVerticalGrid(
         columns = GridCells.Fixed(3),
